37a60e1969a04eb71b5d98f61173f183be01d761
BINGI-KEERTHANA <keerthanabingi19@gmail.com>
2026-07-30 12:42:50 +0530

    Log devices response after login

--- src/pages/Login/Login.tsx
+++ src/pages/Login/Login.tsx
@@ -35,16 +35,13 @@ export default function Login() {
         "access_token",
         response.data.access_token
       );
+      
+      const devicesResponse = await api.get("/api/v1/devices/");
+      console.log("DEVICES RESPONSE:", devicesResponse.data);
       localStorage.setItem(
 	"user_id",
 	response.data.user_id
       );
-      if(response.data.device_id){
-        localStorage.setItem(
-          "device_id",
-	   response.data.device_id
-	);
-}
       addActivity("✅ Logged in successfully");
       navigate("/dashboard");
       
